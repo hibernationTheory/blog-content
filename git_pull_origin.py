23004201a093467edcd4d9d758519b6910a2697a
Engin Arslan <hibernationtheory@gmail.com>
2015-08-23 19:20:44 -0400

    update git scripts

--- git_pull_origin.py
+++ git_pull_origin.py
@@ -1,5 +1,9 @@
 import os
 
-CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
-os.chdir(CURRENT_PATH)
-os.system('git pull origin')
+def main():
+	CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+	os.chdir(CURRENT_PATH)
+	os.system('git pull origin')
+
+if __name__ == '__main__':
+	main()
